b134b24defbdfca098a3d814a9c1eca8e5bbc90c
intai <intai.hg@gmail.com>
2015-02-08 21:57:54 +1300

    Add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('csslint', function () {
-  return gulp.src('**/*.css')
+  return gulp.src(['**/*.less'])
     .pipe($.filter(['**',
+      '!build/**',
       '!node_modules/**',
-      '!bower_components/**']))
+      '!bower_components/**' ]))
     .pipe($.csslint())
     .pipe($.csslint.reporter());
 });
@@ -16,10 +17,18 @@ gulp.task('jshint', function () {
   return gulp.src('**/*.js')
     .pipe($.filter(['**',
       '!*.js',
+      '!build/**',
       '!node_modules/**',
-      '!bower_components/**']))
+      '!bower_components/**' ]))
     .pipe($.jshint())
     .pipe($.jshint.reporter($.jshintStylish));
+});
+
+gulp.task('less', function() {
+  gulp.src('**/*.less')
+    .pipe($.less())
+    .pipe(gulp.dest('build'))
+    .pipe($.livereload());
 });
 
 gulp.task('traceur', function () {
@@ -29,41 +38,47 @@ gulp.task('traceur', function () {
   return gulp.src('**/*.js')
     .pipe($.filter(['**',
       '!*.js',
+      '!build/**',
       '!node_modules/**',
-      '!bower_components/**']))
+      '!bower_components/**' ]))
     .pipe($.addSrc($.traceur.RUNTIME_PATH))
     .pipe($.addSrc(bowerFiles()))
     .pipe($.sourcemaps.init())
     .pipe($.order([
       'traceur-runtime.js',
-      'app.js']))
+      'app.js' ]))
     .pipe(filter)
     .pipe($.traceur())
     .pipe(filter.restore())
     .pipe($.concat('app.js'))
     .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe($.livereload());
   });
 
   gulp.task('clean', function () {
-    return gulp.src('build', { read: false })
-    .pipe($.clean());
+    require('del')('build', function (err, deletedFiles) {
+      console.log('Files deleted:', deletedFiles.join(', '));
+    });
   });
 
   gulp.task('connect', function () {
-    var connect = require('connect');
+    var connect = require('connect'),
+        serveIndex = require('serve-index'),
+        serveStatic = require('serve-static'),
+        livereload = require('connect-livereload');
+
     var app = connect()
-    .use(require('connect-livereload')({
-      port: 35729
-    }))
-    .use(connect.static('.'))
-    .use(connect.directory('.'));
+      .use(livereload({
+        port: 35729 }))
+      .use(serveIndex('.'))
+      .use(serveStatic('.'));
 
     require('http').createServer(app)
-    .listen(3000)
-    .on('listening', function () {
-      console.log('Started connect web server on http://localhost:3000');
-    });
+      .listen(3000)
+      .on('listening', function () {
+        console.log('Started connect web server on http://localhost:3000');
+      });
   });
 
   gulp.task('serve', ['connect'], function () {
@@ -71,18 +86,22 @@ gulp.task('traceur', function () {
   });
 
   gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
+    $.livereload.listen();
 
-    gulp.watch([
-      '*.html',
-      'styles/*.css',
-      'build/*.js'
-      ]).on('change', function (file) {
-        server.changed(file.path);
+    gulp.watch(['**/*.html'])
+      .on('change', function(file) {
+        $.livereload.changed(file.path);
       });
 
-      gulp.watch(['styles/*.css'], ['csslint']);
-      gulp.watch(['gulpfile.js', 'scripts/*.js'], ['jshint', 'traceur']);
-    });
+    gulp.watch(['**/*.less',
+      '!build/**',
+      '!node_modules/**',
+      '!bower_components/**'], ['less']);
 
-    gulp.task('default', ['csslint', 'jshint', 'traceur', 'connect', 'watch']);
+    gulp.watch(['**/*.js',
+      '!build/**',
+      '!node_modules/**',
+      '!bower_components/**'], ['traceur']);
+  });
+
+  gulp.task('default', ['csslint', 'jshint', 'less', 'traceur', 'watch']);
